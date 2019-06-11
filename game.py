d7f6228834b1368f43334a1d737e1c7e83bff09d
Darinmcc <50928612+Darinmcc@users.noreply.github.com>
2019-06-11 15:44:27 -0400

    Create game.py

---
+++ game.py
@@ -0,0 +1,22 @@
+
+import random
+
+print("--------------------")
+print("Welcome to My Rock, Paper, Scissors Game!")
+print("--------------------")
+
+print("Let's begin:")
+
+user_choice = lower(input("Plase choose: 'Rock', 'Paper', 'Scissors':"))
+
+
+options = ["rock", "paper", "scissors"]
+
+if user_choice in options:
+    print("YUP")
+else:
+    print("Nope")
+
+print("Rock, Paper, Scissors, Shoot!")
+
+
